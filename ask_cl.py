107fecdd2bd177f183211849470f8836b3eff2c9
WouterSpekkink <wahsp@tutanota.com>
2023-08-21 16:38:42 +0200

    Changed default model to 16k

--- ask_cl.py
+++ ask_cl.py
@@ -104,7 +104,7 @@ async def start():
       Select(
         id="Model",
         label="OpenAI - Model",
-        values=["gpt-3.5-turbo", "gpt-3.5-turbo-16k", "gpt-4", "gpt-4-32k"],
+        values=["gpt-3.5-turbo-16k", "gpt-4", "gpt-4-32k"],
         initial_index=0,
       ),
       Switch(id="Streaming", label="OpenAI - Stream Tokens", initial=True),
