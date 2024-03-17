aa442800155ce56a4ba843b8b00302fb431efd95
WouterSpekkink <wahsp@tutanota.com>
2024-03-17 14:15:34 +0100

    Updated OpenAIEmbeddings import

--- indexer.py
+++ indexer.py
@@ -1,5 +1,5 @@
 from langchain_community.vectorstores import FAISS
-from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import DirectoryLoader, TextLoader
 import bibtexparser
